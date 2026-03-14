c353ffda6e3ac5570181f5fe35d033ba24df99c4
Juan Darley <juandarley99@gmail.com>
2026-03-14 19:20:54 -0300

    initi project

---
+++ src/main/java/br/com/resenha/Resenha/ResenhaApplication.java
@@ -0,0 +1,13 @@
+package br.com.resenha.Resenha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResenhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResenhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/resenha/Resenha/ResenhaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.resenha.Resenha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResenhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
